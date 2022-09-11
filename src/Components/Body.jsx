9c7e2bc921efb6085212fc8bc560d8f02aced39a
Sol <solemich@gmail.com>
2022-09-11 10:26:55 -0400

    maqueta css

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -3,17 +3,42 @@ import React from "react";
 
 function Content () {
     return (
-        <body className="container">
+     
+        <section className="container">
             <section className="grid1">
-                <div className="contenedor">
-                <h1 className="titulo1">Eres...</h1>
-               </div>
-                
+              <h1 className="titulo1">Eres</h1>
+              <div className="contenedor">
+             <section className="dropdown">
+                <button className="dropbtn">Selecciona</button>
+                <div className="dropdown-content">
+                    <li className="lista">MaleShep</li>
+                    <li className="lista">FemShep</li>
+                    
+                </div>
             </section>
+            <h1 className="titulo1">Clase</h1>
+            <section className="dropdown">
+                <button className="dropbtn">Selecciona</button>
+                <div className="dropdown-content">
+                    <li className="lista">Infiltrado</li>
+                    <li className="lista">tanque</li>
+                    <li className="lista">poto</li>              
+                </div>
+            </section>
+            <section>
+                <h1 className="titulo1"> sjsks</h1>
+            </section>
+            
+            <section className="dropdown">
+                <button className="search">Buscar</button>
+            </section>
+            </div>
+         </section>
             <section className="grid2">
                 <img className="foto" src="./src/img/portada.jpg" alt="logo"/>
             </section>
-         </body>
+         </section>
+    
     )
 }
 
