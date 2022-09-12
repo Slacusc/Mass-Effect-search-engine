4a2937e093dc256b8d797819ecb55a2805c8076c
Sol <solemich@gmail.com>
2022-09-12 12:43:32 -0400

    css

--- src/Components/botones.jsx
+++ src/Components/botones.jsx
@@ -5,13 +5,19 @@ function Nav() {
 <div>
     <section className="nav">
      <img  className="logo" src="./src/img/mel.png" alt="logo" />
-        <section className="buttons">
-            <ul>Juegos</ul>
-            <ul>Multimedia</ul>
-            <ul>Home</ul>
-            <ul>Buscador</ul>
-        </section>
-     {/* <img className="logo" src="./src/img/biow.png" alt="logo"/> */}
+        <ul className="navcontent">
+        <li className="nav1 dropdown2">Juegos
+            <div class="dropdown2-content">
+                <a href="#">me1</a>
+                <a href="#">me2</a>
+                <a href="#">me3</a>
+                </div>
+        </li>
+        <li className="nav1">Multimedia</li>
+        <li className="nav1">Home</li>
+        <li className="nav1">Buscador</li>
+        
+        </ul>
      </section>
 
 
